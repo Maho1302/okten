506179f3cd6c1751067c7e5518cdf78bc337ea74
Maho1302 <g.l.oldi13@gmail.com>
2022-02-04 23:35:12 +0200

    rework

--- JS/lesson2/additional/main.js
+++ JS/lesson2/additional/main.js
@@ -5,6 +5,12 @@
 
 let friends = ["Ivan", "Slavik", "Grisha", "Lera", "Ira"];
 
+if (friends.length >=3) {
+    console.log('Це великий масив.');
+} else {
+    console.log('Це маленький масив.');
+}
+
 if (friends.length === 1) {
     console.log(`Це маленький масив. В ньому ${friends.length} елемент.`);
 } else if (friends.length === 2) {
@@ -17,6 +23,22 @@ if (friends.length === 1) {
     console.log(`Це великий масив. В ньому ${friends.length} елементів.`);
 } else if (friends.length > 6) {
     console.log(`Це великий масив. В ньому ${friends.length} елементів.`);
+};
+
+// - Маємо 3 числа і між ним  нам потрібно знайти те що посередині.
+// Знайти, яке з них є середнім (більше одного, але менше за інше).
+// Перевірити, чи знаходиться перше число між двома іншими.
+
+    let a = +prompt("Введите число:");
+let b = +prompt("Введите число:");
+let c = +prompt("Введите число:");
+debugger
+if (a<b && b<c) {
+    console.log(b);
+} else if (a>b && a<c) {
+    console.log(a);
+} else if (c>a && c<b){
+    console.log(c);
 };
 
 // - Перепишіть конструкцію if з використанням умовного оператора '?':
@@ -34,6 +56,13 @@ let b = 2;
 let result = (a + b <4) ? 'Мало' : 'Багато';
 console.log(result);
 
+// - Маємо будь яке число від -100 , 0 , +100 потрібно перевірити яким є число: позитивним, негативним або нулем
+// напишіть це тернарним оператором
 
+    let ourNum;
+let num = +prompt('Введіть число від -100 до 100:');
 
-
+ourNum =
+    (num > 0) ? 'Число позитивне':
+    (num < 0) ? 'Число негативне': 'Число дорівнює нулю' ;
+console.log(ourNum);
--- JS/lesson2/classwork/main.js
+++ JS/lesson2/classwork/main.js
@@ -2,86 +2,85 @@
 //     Потрібно знайти та вивести максимальне число з тих двох .
 //     Також потрібно врахувати коли введені рівні числа.
 
-// const firstNum = prompt('Введіть перше число:');
-// const secondNum = prompt('Введіть друге число:');
-//
-// if (firstNum < secondNum) {
-//     console.log(`Це число більше: ${secondNum}`);
-// } else if (firstNum > secondNum) {
-//     console.log(`Це число більше: ${firstNum}`);
-// } else {
-//     console.log('Числа однакові!')
-// };
+const firstNum = +prompt('Введіть перше число:');
+const secondNum = +prompt('Введіть друге число:');
+
+if (firstNum < secondNum) {
+    console.log(`Це число більше: ${secondNum}`);
+} else if (firstNum > secondNum) {
+    console.log(`Це число більше: ${firstNum}`);
+} else {
+    console.log('Числа однакові!')
+};
 
 
 // - У першому під'їзді квартири з 1 до 20. У другому з 21 по 48. У третьому з 49 по 90.
 // Користувач вводить номер квартири просто в змінні або через prompt('') .
 //     Програма повинна зазначити, в якому під'їзді знаходиться дана квартира.
 
-// let flatNum = prompt('Введіть номер квартири:');
-//
-// if (flatNum <= 0) {
-//     console.log("Такої квартири немає в цьому домі");
-// } else if (flatNum <=20) {
-//     console.log("Квартира знаходиться у першому під'їзді");
-// } else if (flatNum <=48) {
-//     console.log("Квартира знаходиться у другому під'їзді");
-// } else if (flatNum <=90) {
-//     console.log("Квартира знаходиться у третьому під'їзді");
-// } else {
-//     console.log("Такої квартири немає в цьому домі");
-// };
+let flatNum = prompt('Введіть номер квартири:');
+
+if (flatNum >=1 && flatNum <=20) {
+    console.log("Квартира знаходиться у першому під'їзді");
+} else if (flatNum>=21 && flatNum <=48) {
+    console.log("Квартира знаходиться у другому під'їзді");
+} else if (flatNum>=49 && flatNum <=90) {
+    console.log("Квартира знаходиться у третьому під'їзді");
+} else {
+    console.log("Такої квартири немає в цьому домі");
+};
+
 
 // - Ми маємо змінну number в яку користувач задає числове значення,  якщо змінна рівна 10 вивести
 // повідомлення ВІРНО, якщо змінна не рівна 10 - тоді НЕВІРНО
 
-// let number = prompt('Введіть число:');
-//
-// if (number == 10) {
-//     console.log('ВІРНО');
-// } else {
-//     console.log('НЕВІРНО');
-// };
+let number = !prompt('Введіть число:');
+
+if (number === 10) {
+    console.log('ВІРНО');
+} else {
+    console.log('НЕВІРНО');
+};
 
 // - Ми маємо змінну x в яку користувач задає  значення,  якщо в змінну записане число, вивести 1,
 //     якщо в змінну записали стрінгу вивести 2, якщо булеве значення - 3, якщо об'єкт або масив - 3.
 // якщо в змінну записали щось інше, спрацьовує else
 
-// let x;
-//
-// // x = 3;
-// // x = 'cshos';
-// // x = false;
-// // x = { 1: 1, 2: 2, 3: 3};
-// // x = [1, 2 ,3];
-// // x = undefined;
-//
-//  if (typeof x === 'number') {
-//      console.log(1);
-//  } else if (typeof  x === 'string') {
-//      console.log(2);
-//  } else if (typeof x === "boolean") {
-//      console.log(3);
-//  } else if (typeof x === "object" || typeof x === "arrey") {
-//      console.log(4);
-//  } else {
-//      console.log(5);
-//  };
+let x;
+
+// x = 3;
+// x = 'cshos';
+// x = false;
+// x = { 1: 1, 2: 2, 3: 3};
+// x = [1, 2 ,3];
+// x = undefined;
+
+ if (typeof x === 'number') {
+     console.log(1);
+ } else if (typeof  x === 'string') {
+     console.log(2);
+ } else if (typeof x === "boolean") {
+     console.log(3);
+ } else if (typeof x === "object") {
+     console.log(4);
+ } else {
+     console.log(5);
+ };
 
 // - Задача-гра "чи ми йдемо сьогодні в OKTEN на навчання?".
 //     Змінна, яка характеризує температуру. Якщо температура від +10 до +22 вивести повідомлення
 // що ми йдемо ВЧИТИСЯ .
 //     Якщо інша темпаретура, тоді виводимо повідомлення що сидимо вдома і вчимося ОНЛАЙН
 
-// let temperature = prompt('Введить температуру:');
-//
-// if (temperature <=9) {
-//     console.log('Сидимо вдома і вчимося ОНЛАЙН!');
-// }  else if (temperature <= 22) {
-//     console.log('Ми йдемо ВЧИТИСЯ!');
-// } else {
-//     console.log('Сидимо вдома і вчимося ОНЛАЙН!');
-// };
+let temperature = prompt('Введить температуру:');
+
+if (temperature <=9) {
+    console.log('Сидимо вдома і вчимося ОНЛАЙН!');
+}  else if (temperature>=10 && temperature <= 22) {
+    console.log('Ми йдемо ВЧИТИСЯ!');
+} else {
+    console.log('Сидимо вдома і вчимося ОНЛАЙН!');
+};
 
 // - За допомогою switch case - реалізувати гру - вікторину, де ми вводимо число від 1 до 5. І взалежності від
 //     введеного числа нам дається приз. (Авто, Мото, Телефон, і т.д ) і якщо число не підходить тоді
